caa1ce7b71a12b7d6863a467557f59b503d95c09
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-06-11 01:26:58 +0700

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -234,8 +234,8 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
-rm /etc/xray/$user-tls.json > /dev/null 2>&1
-rm /etc/xray/$user-none.json > /dev/null 2>&1
+#rm /etc/xray/$user-tls.json > /dev/null 2>&1
+#rm /etc/xray/$user-none.json > /dev/null 2>&1
 read -n 1 -s -r -p "Press any key to back on menu"
 
 menu
