d26f46dd54be7b2896b1978b04d4c926504a1bb1
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-06-11 22:53:24 +0700

    Update menu-vless.sh

--- menu-vless.sh
+++ menu-vless.sh
@@ -177,7 +177,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^###vls " "/etc/xray/config.json")
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo ""
         read -n 1 -s -r -p "Press any key to back on menu"
-        menu
+        menu-vless
 	fi
 
 	clear
