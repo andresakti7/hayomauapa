8e89e3e9c16a3841fe079dafd1644419ecf6cdf7
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-06-14 14:40:09 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1280,6 +1280,53 @@ else
 stl="${Error}"
 fi
 clear
+##----- Auto Remove Vmess
+data=( `cat /etc/xray/config.json | grep '^###vms' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d %T"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^###vms $user" "/etc/xray/config.json" | cut -d ' ' -f 3-4 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^###vms $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^###vms $user $exp/,/^},{/d" /etc/xray/grpcconfig.json
+rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
+fi
+done
+
+#----- Auto Remove Vless
+data=( `cat /etc/xray/config.json | grep '^###vls' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d %T"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^###vls $user" "/etc/xray/config.json" | cut -d ' ' -f 3-4 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^###vls $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^###vls $user $exp/,/^},{/d" /etc/xray/grpcconfig.json
+fi
+done
+
+#----- Auto Remove Trojan
+data=( `cat /etc/xray/config.json | grep '^###trs' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d %T"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^###trs $user" "/etc/xray/config.json" | cut -d ' ' -f 3-4 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^###trs $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^###trs $user $exp/,/^},{/d" /etc/xray/grpcconfig.json
+fi
+done
+systemctl restart xray
+clear
 # Getting CPU Information
 vnstat_profile=$(vnstat | sed -n '3p' | awk '{print $1}' | grep -o '[^:]*')
 vnstat -i ${vnstat_profile} >/root/t1
